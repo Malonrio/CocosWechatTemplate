74135187db4650921ee479c1e8bf84fd1ba5221a
赵杰 <zhaojie@fightingx.com>
2018-05-22 17:09:54 +0800

    wx.request 真机测试报错，修改为XMLHttpRequest

--- game.js
+++ game.js
@@ -3,11 +3,13 @@ var Parser = require('libs/xmldom/dom-parser');
 window.DOMParser = Parser.DOMParser;
 require('libs/wx-downloader.js');
 wxDownloader.REMOTE_SERVER_ROOT = "https://dev.zovew.com/Json/";
-wx.request({
-  url: 'https://dev.zovew.com/Json/setting.json',
-  success: (res) => {
-    var response = res.data;
-    window._CCSettings = response;
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {
+    var response = xhr.responseText;
+    window._CCSettings = JSON.parse(response);
     require('main');
-  },
-});
+  }
+};
+xhr.open("GET", 'https://dev.zovew.com/Json/setting.json', true);
+xhr.send();
